4e09ee5403ae20fb481d66dda828a0afffea7ac8
Alan-Miller <alan.ashman@gmail.com>
2018-02-25 00:34:08 -0700

    keyframes files pushed to new repo

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function toggleClass(el, cssClass) {
+    el.classList.toggle(cssClass)    
+}
+
+
+// function toggleClass(el, cssClass) {
+//     if (el.classList.contains(cssClass)) {
+//         el.classList.remove(cssClass);
+//     } 
+//     else {
+//         el.classList.add(cssClass);
+//     }
+// }
